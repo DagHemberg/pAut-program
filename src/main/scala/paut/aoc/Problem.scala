b9adba980b5a8cbc950ac79c09219457096aae22
Dag Hemberg <dag.hemberg@gmail.com>
2022-10-16 15:20:23 +0200

    update to scala 3, revise examplesolution and writing results to file

--- src/main/scala/paut/aoc/Problem.scala
+++ src/main/scala/paut/aoc/Problem.scala
@@ -1,30 +1,36 @@
 package paut.aoc
 
-import scala.util.{Try, Success, Failure}
-import Console._
 import java.time.LocalDate
+import java.util.Locale
+import scala.util.Failure
+import scala.util.Success
+import scala.util.Try
+
+import Console._
 
 /** An abstract class for solving a problem from Advent of Code. 
   * @param year the year the problem is from
   * @param day the day of the year the problem is from
+  * @param part one or two
   * @param expectedExampleSolution the expected solution for the example input
   */
 abstract class Problem[A]
-  (val year: Int, val day: Int, val part: Int)
-  (val example: Example[A]) extends App {
+  (val year: Int, val day: Int)
+  (val part: Int)
+  (val exampleSolution: ExampleSolution[A], i: Int = 1) extends App:
 
-  private val date = LocalDate.of(year, 12, day)
   def name: String
   def solve(data: List[String]): A
-  
-  private def timeSolve(data: Option[List[String]]) = data.map(d => TimedEval.time(solve(d)))
-  override def toString = s"Day $day: $name ($year)"
+  val result = puzzleInput.flatMap(_ => execute)
 
-  private def printlln(x: Any = "")(implicit printResult: Boolean) = 
-    if (printResult) println(x)
+  private val date = LocalDate.of(year, 12, day)
+
+  private def timeSolve(data: Option[List[String]]) = data.map(d => TimedEval.time(solve(d)))
+  
+  override def toString = s"Day $day: $name ($year)"
   
   private def error(msg: String, trim: Boolean = false) =
-    s"[${RED}!${RESET}] ${if (!trim) s"${RED}Something went wrong${RESET} " else ""}$msg"
+    s"[${RED}!${RESET}] ${if !trim then s"${RED}Something went wrong${RESET} " else ""}$msg"
 
   private def success(msg: String) =
     s"[${GREEN}o${RESET}] ${GREEN}$msg${RESET}"
@@ -41,116 +47,97 @@ abstract class Problem[A]
             .map(s => s"      $s")
             .mkString("\n")}""".stripMargin
 
-  private def solvingError(name: String, exception: Throwable)(implicit b: Boolean): Option[TimedEval[A]] = {
-    printlln(s"""|${error(s"when solving the $name problem:")}
-                 |${tinyStack(exception)}""".stripMargin)
+  private def solvingError(name: String, exception: Throwable): Option[TimedEval[A]] = 
+    println(s"""|${error(s"when solving the $name problem:")}
+                |${tinyStack(exception)}""".stripMargin)
     None
-  }
 
-  private def solvingFail(name: String, eval: TimedEval[A])(implicit b: Boolean): Option[TimedEval[A]] = {
-    printlln(f"""|${error(s"${RED}Example failed!${RESET}", trim = true)}
-                 |    Expected: ${CYAN}${example.solution}${RESET}
-                 |    Actual:   ${YELLOW}${eval.result}${RESET}
-                 |    Time: ${eval.duration}%2.6f s""".stripMargin)
+  private def solvingFail(name: String, eval: TimedEval[A]): Option[TimedEval[A]] = 
+    println(s"""|${error(s"${RED}Example failed!${RESET}", trim = true)}
+                |    Expected: ${CYAN}${exampleSolution.sol}${RESET}
+                |    Actual:   ${YELLOW}${eval.result}${RESET}
+                |    Time: ${fmt(eval.duration)} s""".stripMargin)
     None
-  }
 
-  private def solvingSuccess(name: String, eval: TimedEval[A])(implicit b: Boolean): Option[TimedEval[A]] = {
-    printlln(f"""|${success(s"${name.capitalize} solution found!")}
-                 |    Output: ${YELLOW}${eval.result}${RESET}
-                 |    Time: ${eval.duration}%2.6f s%n""".stripMargin)
+  private def solvingSuccess(name: String, eval: TimedEval[A]): Option[TimedEval[A]] = 
+    println(s"""|${success(s"${name.capitalize} solution found!")}
+                |    Output: ${YELLOW}${eval.result}${RESET}
+                |    Time: ${fmt(eval.duration)} s\n""".stripMargin)
     Some(eval)
-  }
 
-  private val resources = os.pwd / "src" / "main" / "resources"
-
-  private def readFile(folder: String, year: Int, file: String) = {
-    Try(os.read.lines(resources / "input" / folder / year.toString / file).toList) match {
+  private def readFile(folder: String, year: Int, file: String) = 
+    Try(os.read.lines(resources / "inputs" / folder / year.toString / file).toList) match
       case Success(lines) => Some(lines)
-      case Failure(err) => {
-        printlln(s"""|${error(s"when reading $file in $folder/$year")}:
-                     |    ${err}""".stripMargin)(true)
+      case Failure(err) => 
+        println(s"""|${error(s"when reading $file in $folder/$year")}:
+                    |    ${err}""".stripMargin)
         None
-      }
-    }
-  }
 
-  private def writeResult(eval: TimedEval[A])(implicit printresult: Boolean): Unit = {
-    val resultsFile = os.home / ".paut" / "aoc" / "results.csv"
-    val newRes = Result(year, day, part, eval.result.toString, eval.duration)
+  private def writeResult(eval: TimedEval[A]): Unit = 
+    case object NoResult extends Throwable
+    val resultsFile = resources / "results.csv"
+    val newRes = Result(year, day, part.toInt, eval.result.toString, eval.duration)
     val now = java.time.LocalDateTime.now()
 
-    if (!os.exists(resultsFile)) os.write(resultsFile, "", createFolders = true)
+    def canUpdate(oldRes: Result) = 
+      oldRes.solution == eval.result.toString && oldRes.time > eval.duration
 
-    os.read.lines(resultsFile).toList
+    def updateFile(or: Result, nr: Result) = 
+      os.write.over(resultsFile, os.read(resultsFile).replace(or.raw, nr.raw))
+
+    if !os.exists(resultsFile) then os.write(resultsFile, "", createFolders = true)
+
+    val maybeRes = os
+      .read.lines(resultsFile)
       .find(_.startsWith(s"$year;$day;$part"))
-      .map(str => Result.parse(str.trim)) match {
-        case None => os.write.append(resultsFile, newRes.raw)
-        case Some(result) => {
-          os.write.over(
-            resultsFile, 
-            os.read(resultsFile).replace(result.raw, 
-              if (result.submitted && result.solution == eval.result.toString && result.time > eval.duration) {
-                result.copy(timestamp = now, time = eval.duration).raw
-              } else {
-                newRes.raw
-              }
-            )
-          )
-        }
-      }
-  }
+      .toRight(left = NoResult)
+      .flatMap(str => Result.tryParse(str.trim).toEither)
 
-  private val primaryExampleInput = readFile("examples", year, s"$day-primary.txt")
-  private val secondaryExampleInput = readFile("examples", year, s"$day-secondary.txt")
-  private val puzzleInput = readFile("puzzles", year, s"$day.txt")
+    maybeRes match
+      case Left(NoResult) => os.write.append(resultsFile, s"${newRes.raw}\n")
+      case Left(err) => 
+        println(s"""|${error(s"when updating the results file:")}
+                    |    ${err}""".stripMargin)
+      case Right(oldRes) =>
+        if oldRes.submitted then 
+          if canUpdate(oldRes) 
+          then updateFile(oldRes, oldRes.copy(time = eval.duration, timestamp = now))
+        else
+          updateFile(oldRes, newRes)
+  
+  private def exampleInput(i: Int) = readFile("examples", year, f"$day%02d-$i.txt")
+  private lazy val puzzleInput = readFile("puzzles", year, s"$day.txt")
 
   /** Attempts to solve the given problem using the given data. 
-    * @param printResult Given boolean that decides whether to print the result or not. Defaults to true. 
     * @return The solution (if it was found), along with the time it took to solve it, or None if no solution was found or if the test case didn't pass, given that an example was provided.
     */
-  def execute(implicit printResult: Boolean = true) = {
-    for (i <- 1 to 100) printlln()
-    printlln(info(toString))
+  def execute =
+    for i <- 1 to 100 do println()
+    println(info(toString))
 
-    def solveExample(example: Option[List[String]], solution: A) = {
+    def solveExample(example: Option[List[String]], solution: A) = 
       Try(timeSolve(example)).fold(
         err => solvingError("example", err),
-        opt => opt flatMap {
-          exampleEval => {
-            if (exampleEval.result == solution) solvingSuccess("example", exampleEval)
-            else solvingFail("example", exampleEval)
-          }
+        opt => opt flatMap { exampleEval => 
+          if exampleEval.result == solution then solvingSuccess("example", exampleEval)
+          else solvingFail("example", exampleEval)
         }
       )
-    }
 
-    def solvePuzzle = {
-      printlln(info("Evaluating puzzle input..."))
+    def solvePuzzle = 
+      println(info("Evaluating puzzle input..."))
       Try(timeSolve(puzzleInput)).fold(
         err => solvingError("puzzle", err),
         opt => opt flatMap { puzzleEval => solvingSuccess("puzzle", puzzleEval) }
       )
-    }
 
-    val result = example match {
-      case skip: Skip.type => {
-        printlln(info("No example provided, evaluating puzzle input..."))
+    val result = exampleSolution match
+      case Skip => 
+        println(info("No example provided, evaluating puzzle input..."))
         solvePuzzle
-      }
-      case ex => {
-        printlln(info("Evaluating example input..."))
-        val (exampleInput, solution) = ex match {
-          case PrimaryEx(solution) => (primaryExampleInput, solution)
-          case SecondaryEx(solution) => (secondaryExampleInput, solution)
-        }
-        solveExample(exampleInput, solution).flatMap(_ => solvePuzzle)
-      }
-    }
+      case other => 
+        println(info("Evaluating example input..."))
+        solveExample(exampleInput(i), exampleSolution.sol).flatMap(_ => solvePuzzle)
 
     result.foreach(writeResult)
     result
-  }
-
-  val result = puzzleInput.flatMap(_ => execute)
-}
--- src/main/scala/paut/aoc/other.scala
+++ src/main/scala/paut/aoc/other.scala
@@ -1,34 +1,29 @@
 package paut.aoc
 
-trait Example[A] { val solution: A }
+import java.util.Locale
 
-case object Skip extends Example[Any] { val solution = None }
-case class PrimaryEx[A](solution: A) extends Example[A]
-case class SecondaryEx[A](solution: A) extends Example[A]
+private [aoc] val resources = os.home / ".paut" / "aoc"
+private [aoc] def fmt(time: Double) = String.format(Locale.US, "%2.6f", time)
+
+implicit class ExampleSolution[A](private [aoc] val sol: A)
+
+/** Used in the constructor of a subclass to the abstract Problem class 
+  * in order to skip evaluation of the example input */
+case object Skip extends ExampleSolution[Any](None)
 
 /** A simple wrapper class that includes the result of an evaluation and the time (in seconds) it took to evaluate it
  * @param result The final evaluation
  * @param time Time elapsed while evaluating, in seconds
  */
 case class TimedEval[A](duration: Double, result: A)
-object TimedEval {
+object TimedEval:
   /** Times the evaluation of a block of code */
-  def time[A](block: => A): TimedEval[A] = {
+  def time[A](block: => A): TimedEval[A] =
     val start = System.nanoTime()
     val result = block
     val duration = (System.nanoTime() - start) / 1E9
     TimedEval(duration, result)
-  }
 
-  def logTime[A](block: => A) = {
-    val t = time(block)
-    println(t.duration)
-    t.result
-  }
-}
-
-object Testing {
-  def read(folder: String, year: String, day: String) = {
-    os.read.lines(os.home / ".paut" / "aoc" / "inputs" / folder / year / s"$day.txt").toList
-  }
-}
+object Testing:
+  def read(folder: String, year: String, day: String) = 
+    os.read.lines(resources / "inputs" / folder / year / s"$day.txt").toList
