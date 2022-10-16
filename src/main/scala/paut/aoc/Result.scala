24a3e67dad2172b206c8172a56e5f3aad3a81ea0
Dag Hemberg <dag.hemberg@gmail.com>
2022-10-16 15:18:43 +0200

    added tryParse, updated datetime formatting

--- src/main/scala/paut/aoc/Result.scala
+++ src/main/scala/paut/aoc/Result.scala
@@ -1,7 +1,9 @@
 package paut.aoc
 
-import java.time.{LocalDate, LocalDateTime}
+import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import scala.util.Try
+
 import Console._
 
 /** A class for representing the result of solving, or an attempt at solving, a problem from Advent of Code.
@@ -21,11 +23,11 @@ case class Result(
     time: Double,
     timestamp: LocalDateTime = LocalDateTime.now(),
     submitted: Boolean = false
-) {
-  override def toString = {
+):
+  override def toString = 
     val from = s"AoC $year day $day - Part $part"
-    val latest = s"Latest run: ${timestamp.format(DateTimeFormatter.ISO_DATE_TIME)}"
-    val dur = s"Fastest run: ${(f"${YELLOW}${time}%.5fs${RESET}")}"
+    val latest = s"Latest run: ${timestamp.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)}"
+    val dur = s"Fastest run: ${YELLOW}${fmt(time)}${RESET}"
     val sol = s"Solution: $solution"
     val stat = s"Status: ${if (submitted) s"${GREEN}Submitted${RESET}" else s"${RED}Not submitted${RESET}"}"
     
@@ -35,24 +37,24 @@ case class Result(
         |$sol
         |$stat
         |""".stripMargin
-  }
 
-  def raw = s"$year;$day;$part;$solution;$time;$timestamp;$submitted"
-}
+  def raw = s"$year;$day;$part;$solution;${fmt(time)};$timestamp;$submitted"
 
-object Result {
-  /** Parses a string in the format `year;day;part;solution;time;timestamp;submitted` to a Result */
-  def parse(str: String): Result = {
-    val parts = str.split(";").toList
-    require(parts.length == 7, "Invalid format")
+object Result:
+  /** Parses a string in the format `year;day;part;solution;time;timestamp;submitted` to a `Result` */
+  def parse(str: String) = 
+    val parts = str.split(";").toVector
     Result(
       year = parts(0).toInt,
       day = parts(1).toInt,
       part = parts(2).toInt,
       solution = parts(3),
       time = parts(4).toDouble,
-      timestamp = LocalDateTime.parse(parts(5), DateTimeFormatter.ISO_DATE_TIME),
+      timestamp = LocalDateTime.parse(parts(5), DateTimeFormatter.ISO_LOCAL_DATE_TIME),
       submitted = parts(6).toBoolean
     )
-  }
-}
+
+  /** Parses a string in the format `year;day;part;solution;time;timestamp;submitted` to a `Try[Result]`
+   * @return `Success(Result)` if the string was successfully parsed, `Failure(e)` otherwise
+   */
+  def tryParse(str: String) = Try(parse(str))
